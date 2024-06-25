fb370a1c0f5ee5ca91bbbfffd133026912744b11
fawzy20 <fawzy-elsam@outlook.fr>
2024-06-25 22:34:21 +0200

    ajout de requette http

--- src/app/models/connexion/connexion.component.ts
+++ src/app/models/connexion/connexion.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Utilisateur } from 'src/app/dataModels/utilisateur';
 import { NavigationService } from 'src/app/services/navigation-service.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-connexion',
@@ -7,13 +10,21 @@ import { NavigationService } from 'src/app/services/navigation-service.service';
   styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent {
-
+  utilisateur: Utilisateur;
   constructor(
+    private route: Router,
+    private userService: UserService,
     private navigation: NavigationService
-  ){}
+  ) { this.utilisateur = new Utilisateur() }
 
   toSubscribe(page: string) {
     this.navigation.moveNewPage(page)
+  }
+
+  onSubmit() {
+    this.userService.login(this.utilisateur).subscribe(() => {
+      this.route.navigate(['/admin']);
+    })
   }
 
 }
